539d91da02e1def1ff4f9179a480c28683df5b51
Lucas Medeiros <lucamedeir@gmail.com>
2019-07-23 20:14:33 -0300

    removed one of the buttons and added number of cycles

--- switch_controller/switch_controller.ino
+++ switch_controller/switch_controller.ino
@@ -1,3 +1,9 @@
+#include <avr/eeprom.h>
+
+#define ADDRESS_COMMAND 0
+#define ADDRESS_CYCLES ADDRESS_COMMAND + 1
+uint32_t numberOfCycles = 0;
+
 #define RELAY_1 9
 #define RELAY_2 8
 #define RELAY_3 7
@@ -8,18 +14,14 @@
 #define RELAY_8 2
 #define RELAY_ALL 0
 
-#define CLOCKWISE_PIN 10
 #define ANTICLOCKWISE_PIN 11
 
-char presentCommand = 0;
+int8_t presentCommand = 0;
 
-char buttonValueClockwise = 1;
-char previousButtonValueClockwise = 1;
+uint8_t buttonValueAntiClockwise = 1;
+uint8_t previousButtonValueAntiClockwise = 1;
 
-char buttonValueAntiClockwise = 1;
-char previousButtonValueAntiClockwise = 1;
-
-int timeDelay = 100;
+uint8_t timeDelay = 50;
 
 // Here we set all relay pins as output and and the relay as off.
 // and then turn on serial communication and send a code to say it is ready ('o').
@@ -33,20 +35,29 @@ void setup() {
   pinMode(RELAY_7,OUTPUT);
   pinMode(RELAY_8,OUTPUT);
 
-  pinMode(CLOCKWISE_PIN,INPUT);
   pinMode(ANTICLOCKWISE_PIN,INPUT);
 
+  while (!eeprom_is_ready());
+
+  presentCommand = eeprom_read_byte((uint8_t*)ADDRESS_COMMAND);
+  numberOfCycles = eeprom_read_dword((uint32_t*)ADDRESS_CYCLES);
+  
   // reset all relay
   click(-1);
 
   Serial.begin(9600);
-  Serial.print('o');
-  
+  Serial.print("o ");
+  Serial.println(presentCommand+1);
+  Serial.print("# of EEPROM cycles: ");
+  Serial.println(numberOfCycles);
+  if(numberOfCycles>90000) {
+    Serial.println("ALERT: The number of write/erase cycles at the EEPROM is over 90k.\n The max number allowed is 100k but it is recommended to change the arduino chip nonetheless.");
+  }
 }
 
 // This function in turn off all relay except the relay k
-void click(int relay_k) {
-  for(int relay = 2; relay <= 9; relay++) {
+void click(int8_t relay_k) {
+  for(int8_t relay = 2; relay <= 9; relay++) {
       digitalWrite(relay,HIGH);
   }
   if(relay_k >= 2 and relay_k <= 9)
@@ -54,10 +65,13 @@ void click(int relay_k) {
 }
 
 // activate for a brief time just on relay
-void change(char command,int t) {
+void change(int8_t command,uint8_t t) {
       digitalWrite(9-command,LOW);
       delay(t);
       digitalWrite(9-command,HIGH);
+      eeprom_write_byte((uint8_t*)ADDRESS_COMMAND,(uint8_t)presentCommand);
+      numberOfCycles++;
+      eeprom_write_dword((uint32_t*)ADDRESS_CYCLES,numberOfCycles);
 }
 
 // this function detects a command a do something.
@@ -81,24 +95,18 @@ void loop() {
 	// ....
 	// pin 3 - relay 7
 	// pin 2 - relay 8
-      change(9-presentCommand,timeDelay);
-      Serial.print((int)presentCommand+1);
+      change(presentCommand,timeDelay);
+      Serial.print("serial: ");
+      Serial.println((int)presentCommand+1);
     } else if (command != '\n') Serial.print('i');
-  }
-
-  previousButtonValueClockwise = buttonValueClockwise;
-  buttonValueClockwise = digitalRead(CLOCKWISE_PIN);
-  if(buttonValueClockwise == previousButtonValueClockwise-1) {
-    if(++presentCommand>5)presentCommand = 0;
-    change(presentCommand,timeDelay);
-    Serial.print((int)presentCommand+1);
   }
 
   previousButtonValueAntiClockwise = buttonValueAntiClockwise;
   buttonValueAntiClockwise = digitalRead(ANTICLOCKWISE_PIN);
   if(buttonValueAntiClockwise == previousButtonValueAntiClockwise-1) {
-    if(--presentCommand<0) presentCommand = 5;
+    if(--presentCommand<0) presentCommand = 6;
     change(presentCommand,timeDelay);
-    Serial.print((int)presentCommand+1);
+    Serial.print("button: ");
+    Serial.println((int)presentCommand+1);
   }
 }
